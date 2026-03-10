a803c1e8e8d567be40f5ab59fcfb2bc1e6d65849
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-03-10 15:57:22 -0300

    app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -212,44 +212,44 @@ def gerar_pdf_cliente(cliente, vendas_cliente):
 
     if len(vendas_cliente) > 0:
 
-        resumo = (
-            vendas_cliente
-            .groupby(["DESC PRODUTO","LINHA"])[["QTDE","VALOR"]]
-            .sum()
-            .reset_index()
-            .sort_values("VALOR", ascending=False)
-        )
+resumo = (
+    vendas_cliente
+    .groupby(["DESC PRODUTO","LINHA"])[["QTDE","VALOR"]]
+    .sum()
+    .reset_index()
+    .sort_values("VALOR", ascending=False)
+)
 
-        total_valor = resumo["VALOR"].sum()
-        total_qtd = resumo["QTDE"].sum()
-        total_skus = resumo["DESC PRODUTO"].nunique()
+total_valor = resumo["VALOR"].sum()
+total_qtd = resumo["QTDE"].sum()
+total_skus = resumo["DESC PRODUTO"].nunique()
 
-        resumo_comercial = [
+resumo_comercial = [
 
-            ["Total SKUs Comprados", total_skus],
-            ["Total Unidades", int(total_qtd)],
-            ["Valor Total Comprado", f"R$ {total_valor:,.2f}"]
+    ["Total Produtos Comprados", total_skus],
+    ["Total Unidades", int(total_qtd)],
+    ["Valor Total Comprado", f"R$ {total_valor:,.2f}"]
 
-        ]
+]
 
-        tabela_resumo = Table(resumo_comercial)
+tabela_resumo = Table(resumo_comercial)
 
-        elementos.append(Spacer(1,10))
-        elementos.append(tabela_resumo)
-        elementos.append(Spacer(1,20))
+elementos.append(Spacer(1,10))
+elementos.append(tabela_resumo)
+elementos.append(Spacer(1,20))
 
-        dados_produtos = [["Produto","Linha","Quantidade","Valor"]]
+dados_produtos = [["Produto","Linha","Quantidade","Valor"]]
 
-        for _,row in resumo.iterrows():
+for _,row in resumo.iterrows():
 
-            dados_produtos.append([
+    dados_produtos.append([
 
-                row["DESC PRODUTO"],
-                row["LINHA"],
-                int(row["QTDE"]),
-                f"R$ {row['VALOR']:,.2f}"
+        row["DESC PRODUTO"],
+        row["LINHA"],
+        int(row["QTDE"]),
+        f"R$ {row['VALOR']:,.2f}"
 
-            ])
+    ])
 
         tabela_produtos = Table(dados_produtos)
 
@@ -572,5 +572,6 @@ st.download_button(
 st.subheader("Base de Clientes")
 
 st.dataframe(df_filtrado, use_container_width=True)
+
 
 
