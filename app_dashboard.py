27f70dc017e2e524af0db7c5d0f492bda1d02d65
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-03-11 08:30:07 -0300

    app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -409,7 +409,8 @@ else:
 
     buffer.seek(0)
 
-return buffer
+    return buffer
+    
 # =========================
 # SIDEBAR
 # =========================
@@ -710,6 +711,7 @@ st.download_button(
 st.subheader("Base de Clientes")
 
 st.dataframe(df_filtrado, use_container_width=True)
+
 
 
 
