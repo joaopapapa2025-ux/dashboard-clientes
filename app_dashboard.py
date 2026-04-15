a739f2c675280770bab65eb8eba891b22e80330f
joaopapapa2025-ux <joao.papapa2025@gmail.com>
2026-04-15 10:27:53 -0300

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -815,7 +815,7 @@ if cliente_sel != "":
 st.title("Dashboard Inside Sales - PAPAPÁ")
 
 # Você pode alterar a data/hora manualmente aqui sempre que atualizar os números
-data_atualizacao = "01/04/2026" 
+data_atualizacao = "15/04/2026" 
 st.markdown(f"🕒 *Última atualização: {data_atualizacao}*")
 
 # =========================
